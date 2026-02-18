2dd4b4ffe957e88f002e2fd966b82571eef28a1a
Hari-gohil <gohilhari2006@gmail.com>
2026-02-19 01:03:21 +0530

    change baseURL

--- frontend2/src/assets/services/SellerApi.js
+++ frontend2/src/assets/services/SellerApi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://ecommerce-zfl4.onrender.com/api", // backend port
-    baseURL: "http://localhost:3000/api"
+  baseURL: "https://ecommerce-zfl4.onrender.com/api", // backend port
+    // baseURL: "http://localhost:3000/api"
 });
 
 /* ================= TOKEN INTERCEPTOR ================= */
